5a7a4f58bed162b7642a5ced5d0cd7aa75d085fd
Sedrik Suurmets <sesuur@ttu.ee>
2020-09-26 11:54:45 +0300

    #16 created interface for listing data, listing-view.component.html uses data from listing-view.component.ts

---
+++ src/app/listingProperties/Listing.ts
@@ -0,0 +1,22 @@
+export interface Listing {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+  price: number;
+  location: string;
+  carId: string;
+  bodyType: string;
+  brand: string;
+  model: string;
+  color: string;
+  gearbox: string;
+  fuelType: string;
+  driveType: string;
+  enginePower: number;
+  engineSize: string;
+  mileage: number;
+  year: number;
+  ownerName: string;
+  ownerNumber: string;
+}
