adda6f44233101a17b88a2e85cd601c176b058de
Kaspar Ustav <kausta@ttu.ee>
2020-10-04 19:46:53 +0300

    Added path to get individual listing by id under 'listings/:id'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { DummyComponent } from './dummy/dummy.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {PostNewListingComponent} from './post-new-listing/post-new-listing.component';
+import {ListingComponent} from './listing/listing.component';
+import {ListingViewComponent} from './listing-view/listing-view.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path: 'home', component: HomePageComponent},
   { path: 'allcars', component: DummyComponent},
-  { path: 'post', component: PostNewListingComponent},
+  {path: 'post', component: PostNewListingComponent},
+  {path: 'listings', component: ListingComponent},
+  {path: 'listings/:id', component: ListingViewComponent},
   { path: 'profile', component: ProfilePageComponent}
 ];
 
