25ed4f99bb9c8340c1d140c819640b2d4b8f76e2
Kaspar Ustav <kausta@ttu.ee>
2020-10-04 19:45:00 +0300

    Added path to see all listings under '/listings'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DummyComponent } from './dummy/dummy.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {PostNewListingComponent} from './post-new-listing/post-new-listing.component';
+import {ListingComponent} from './listing/listing.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path: 'home', component: HomePageComponent},
   { path: 'allcars', component: DummyComponent},
-  {path: 'post', component: PostNewListingComponent}
+  {path: 'post', component: PostNewListingComponent},
+  {path: 'listings', component: ListingComponent}
   ];
 
 @NgModule({
