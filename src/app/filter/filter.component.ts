13c0862811d5c523765e8011f2fc0f6c6cd0b362
Ewert Ubaleht <ewubal@ttu,ee>
2020-10-05 16:36:40 +0300

    Dropdown menu has options from db, but with duplicates.

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {CarService} from '../car.service';
+import {Car} from '../car';
+import {ListingItem} from '../listingItem';
+import {ListingItemService} from '../listingItem.service';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
-export class FilterComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void {
+export class FilterComponent implements OnInit {
+  constructor( private carService: CarService, private listingItemService: ListingItemService) { }
+
+  carItems: Car[];
+
+  listingItems: ListingItem[];
+
+  getCarItems(): void {
+    this.carService.getCars().subscribe(carItems => this.carItems = carItems);
   }
 
+  getListingItems(): void {
+    this.listingItemService.getListings().subscribe(listingItems => this.listingItems = listingItems);
+  }
+
+  ngOnInit(): void {
+    this.getCarItems();
+    this.getListingItems();
+  }
 }
