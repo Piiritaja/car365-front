d6e95fc1e2aaf53e368bfd90e1e51484d20678c1
Mikk Järvis <mikjar@ttu.ee>
2020-10-09 18:06:09 +0300

    #33 saving chosen filter values to sessionStorage

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -11,6 +11,20 @@ export class FilterComponent implements OnInit {
   constructor(private listingItemService: ListingItemService) { }
 
   listingParams: any[] = [];
+  selectedColor: any;
+  selectedLocation: any;
+  selectedDriveType: any;
+  selectedGearBoxType: any;
+  selectedFuel: any;
+  selectedModel: any;
+  selectedMake: any;
+  selectedBodyType: any;
+  selectedYearStart: string;
+  selectedYearEnd: string;
+  selectedPowerStart: string;
+  selectedPowerEnd: string;
+  selectedPriceEnd: string;
+  selectedPriceStart: string;
 
   getParamStrings(): void {
     this.listingItemService.getParams()
@@ -18,6 +32,20 @@ export class FilterComponent implements OnInit {
         data => {
       this.listingParams = data;
     });
+  }
+
+  saveSelectedParams(): void {
+    sessionStorage.setItem('color', this.selectedColor);
+    sessionStorage.setItem('location', this.selectedLocation);
+    sessionStorage.setItem('driveType', this.selectedDriveType);
+    sessionStorage.setItem('gearBoxType', this.selectedGearBoxType);
+    sessionStorage.setItem('fuel', this.selectedFuel);
+    sessionStorage.setItem('model', this.selectedModel);
+    sessionStorage.setItem('make', this.selectedMake);
+    sessionStorage.setItem('bodyType', this.selectedBodyType);
+    sessionStorage.setItem('yearRange', this.selectedYearStart + ' ' + this.selectedYearEnd);
+    sessionStorage.setItem('powerRange', this.selectedPowerStart + ' ' + this.selectedPowerEnd);
+    sessionStorage.setItem('priceRange', this.selectedPriceEnd + ' ' + this.selectedPriceStart);
   }
 
   ngOnInit(): void {
