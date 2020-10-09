a5ecb0e06179aed8a65929af26c64f63948ed1b6
Ewert Ubaleht <ewubal@ttu,ee>
2020-10-09 20:55:48 +0300

    all Cars path fixed and now displays listing of all cars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DummyComponent } from './dummy/dummy.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {PostNewListingComponent} from './post-new-listing/post-new-listing.component';
 import {ListingComponent} from './listing/listing.component';
 import {ListingViewComponent} from './listing-view/listing-view.component';
 import {SearchListingsComponent} from './search-listings/search-listings.component';
+import {AllCarsComponent} from './all-cars/all-cars.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path: 'home', component: HomePageComponent},
-  { path: 'allcars', component: DummyComponent},
+  { path: 'allCars', component: AllCarsComponent},
   {path: 'post', component: PostNewListingComponent},
   {path: 'listings', component: ListingComponent},
   {path: 'listings/:id', component: ListingViewComponent},
-  {path: 'search', component: SearchListingsComponent}
+  {path: 'search', component: SearchListingsComponent},
   ];
 
 @NgModule({
