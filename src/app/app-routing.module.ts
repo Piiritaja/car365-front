e61a3aa458362cbdd771457b206089ccfc0505e4
Ewert Ubaleht <ewubal@ttu,ee>
2020-12-11 22:27:23 +0200

    settings page save changes button fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {ProfilePageComponent} from './profile-page/profile-page.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostNewListingComponent} from './post-new-listing/post-new-listing.component';
 import {ListingComponent} from './listing/listing.component';
@@ -9,6 +8,9 @@ import {SearchListingsComponent} from './search-listings/search-listings.compone
 import {AllCarsComponent} from './all-cars/all-cars.component';
 import {EditListingComponent} from './edit-listing/edit-listing.component';
 import {AuthGuard} from './helpers/auth.guard';
+import {ProfilePageListingsComponent} from './profile-page-listings/profile-page-listings.component';
+import {ProfilePageFavoritesComponent} from './profile-page-favorites/profile-page-favorites.component';
+import {ProfilePageSettingsComponent} from './profile-page-settings/profile-page-settings.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
@@ -18,7 +20,9 @@ const routes: Routes = [
   {path: 'listings', component: ListingComponent},
   {path: 'listings/:id', component: ListingViewComponent},
   {path: 'search', component: SearchListingsComponent},
-  {path: 'profile', component: ProfilePageComponent, canActivate: [AuthGuard]},
+  {path: 'profile/listings', component: ProfilePageListingsComponent, canActivate: [AuthGuard]},
+  {path: 'profile/favorites', component: ProfilePageFavoritesComponent, canActivate: [AuthGuard]},
+  {path: 'profile/settings', component: ProfilePageSettingsComponent, canActivate: [AuthGuard]},
   {path: 'listings/edit/:id', component: EditListingComponent},
   {path: 'search/:i', component: SearchListingsComponent},
   {path: 'allCars/:i', component: AllCarsComponent}
